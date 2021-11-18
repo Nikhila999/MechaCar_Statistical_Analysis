c00dd53aba8e2f6bf8f4669d0ac4d85de9ef630f
Nikhila999 <40877603+Nikhila999@users.noreply.github.com>
2021-11-18 16:05:43 -0600

    updated r file

--- MechaCarChallenge.r
+++ MechaCarChallenge.r
@@ -7,6 +7,8 @@ lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AW
 
 summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD,data=mechacars_table))
 
+plot(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD,data=mechacars_table) )
+
 # Deliverable 2: Create Visualizations for the Trip Analysis
 suspension_coil_table <- read.csv('Suspension_Coil.csv',check.names = F,stringsAsFactors = F)
 
@@ -18,17 +20,17 @@ lot_summary <- suspension_coil_table %>% group_by(Manufacturing_Lot) %>% summari
 
 # Deliverable 3: T-Tests on Suspension Coils
 # T-Tests on Suspension Coils
-t.test(suspension_coil_table$PSI, mu=mean(suspension_coil_table$PSI))
+t.test(suspension_coil_table$PSI, mu=1500)
 
 # T-Tests on Suspension Coils for Lot1
 lot1 = suspension_coil_table %>% filter(Manufacturing_Lot=='Lot1')
-t.test(lot1$PSI, mu=mean(suspension_coil_table$PSI))
+t.test(lot1$PSI, mu=1500)
 
 # T-Tests on Suspension Coils for Lot2
 lot2 = suspension_coil_table %>% filter(Manufacturing_Lot=='Lot2')
-t.test(lot2$PSI, mu=mean(suspension_coil_table$PSI))
+t.test(lot2$PSI, mu=1500)
 
 # T-Tests on Suspension Coils for Lot3
 lot3 = suspension_coil_table %>% filter(Manufacturing_Lot=='Lot3')
-t.test(lot3$PSI, mu=mean(suspension_coil_table$PSI))
+t.test(lot3$PSI, mu=1500)
 
